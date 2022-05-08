9b5862d5832ea539fca1b09d9651fcca29745784
Ubuntu <ubuntu@ip-172-31-82-252.ec2.internal>
2022-05-08 15:35:36 +0000

    Update tokenizer toraining script

--- study_on_T5/HuggingFace_Language_model_training_examples/01_train_torknizer.py
+++ study_on_T5/HuggingFace_Language_model_training_examples/01_train_torknizer.py
@@ -1,24 +1,36 @@
+import os
+
 from datasets import load_dataset
-from tokenizers import trainers, Tokenizer, normalizers, ByteLevelBPETokenizer
+from tokenizers import ByteLevelBPETokenizer
 
-# load dataset
-dataset = load_dataset("oscar", "unshuffled_deduplicated_no", split="train")
 
-# Instantiate tokenizer
-tokenizer = ByteLevelBPETokenizer()
-
-def batch_iterator(batch_size=1000):
+def batch_iterator(batch_size: int = 1000):
     for i in range(0, len(dataset), batch_size):
-        yield dataset[i: i + batch_size]["text"]
+        yield dataset[i : i + batch_size]["text"]
 
-# Customized training
-tokenizer.train_from_iterator(batch_iterator(), vocab_size=50265, min_frequency=2, special_tokens=[
-    "<s>",
-    "<pad>",
-    "</s>",
-    "<unk>",
-    "<mask>",
-])
 
-# Save files to disk
-tokenizer.save("./norwegian-roberta-base/tokenizer.json")
+if __name__ == "__main__":
+    # load dataset, total size 6.5 GB
+    dataset = load_dataset("oscar", "unshuffled_deduplicated_no", split="train")
+
+    # Instantiate tokenizer
+    tokenizer = ByteLevelBPETokenizer()
+    # 　Special Tokens used in ByteLevelBPETokenizer
+    special_tokens = ["<s>", "<pad>", "</s>", "<unk>", "<mask>"]
+    # Customized training
+    tokenizer.train_from_iterator(
+        iterator=batch_iterator(),
+        vocab_size=50265,
+        min_frequency=2,
+        special_tokens=special_tokens,
+    )
+    # print(tokenizer.all_special_tokens)
+    # print(tokenizer.all_special_ids)
+
+    # Create a directory to store the tokenizer file
+    data_dir = "./norwegian-roberta-base"
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    # Save files to disk
+    tokenizer.save(data_dir + "/tokenizer.json")
